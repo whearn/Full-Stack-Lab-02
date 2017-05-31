67c78a6c85be78b247338e52709567545b27103b
whearn <wchearn00@gmail.com>
2017-05-31 14:21:46 -0400

    Correct Grammer on Second to Last Line of the Song

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ for (var i = 0; i < friends.length; i++) {
     for (var j = 99; j > 0; j--) {
         if (j === 1) {
             console.log(j + ' line of code in the file, ' + j + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+        } else if (j === 2) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' line of code in the file');
         } else {
             console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file');
         }
